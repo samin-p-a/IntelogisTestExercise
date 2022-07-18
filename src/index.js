8aee2024ac4d8c4b371dc53c52d025e75bada55c
Piter <saminpa@gmail.com>
2022-07-18 09:51:16 +0300

    В проект подключен редакс, апи-эмулятор и App на правильном расоложении.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+
+import Api from './services/Api';
+import App from './Components/App';
+
+
+const store = createStore(reducer);
+const api = new Api(store);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
