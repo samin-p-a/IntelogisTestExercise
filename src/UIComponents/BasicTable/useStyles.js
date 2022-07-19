f122b8ac21a4c255ea4e269a6be52061087e5017
Piter <saminpa@gmail.com>
2022-07-19 06:30:18 +0300

    Еще один мелкий баг.

--- src/UIComponents/BasicTable/useStyles.js
+++ src/UIComponents/BasicTable/useStyles.js
@@ -1,16 +1,6 @@
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles((theme) => ({
-  grow: {
-    flexGrow: 1,
-  },
-  title: {
-    display: 'block',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1323,
-    display:'none',
-  },
 }));
 
 export default useStyles;
